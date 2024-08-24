4858441898fed5daad7dcf02175148a1e30d6fb8
Rishav chand <108648353+rishavchand@users.noreply.github.com>
2024-08-24 19:54:16 +0530

    Update FeaturedCourses.tsx

--- src/app/components/FeaturedCourses.tsx
+++ src/app/components/FeaturedCourses.tsx
@@ -32,7 +32,7 @@ export default function FeaturedCourses(){
                             <div className="p-4 sm:p-6 flex flex-col items-center text-center flex-grow">
                                 <p  className="text-lg sm:text-xl text-black mt-4 mb-2 dark:text-neutral-200">{course.title}</p>
                                 <p  className="text-sm text-neutral-600 dark:text-neutral-400 flex-grow">{course.description}</p>
-                                <Link href={'/course/${course.slug}'}>Learn more</Link>
+                                <Link href={`/course/${course.slug}`}>Learn more</Link>
                             </div>
                         </BackgroundGradient>
                      
